21d74131b0053cac5dc8b1570b4e2932e46eefae
Jerry Haaser <jerry.haaser@hotmail.com>
2020-01-08 11:40:12 -0800

    added CoreDataStack

---
+++ FoodieFun/FoodieFun/Models/CoreDataStack.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataStack.swift
+//  FoodieFun
+//
+//  Created by Jerry haaser on 1/8/20.
+//  Copyright © 2020 Jerry haaser. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    private init() {
+        
+    }
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Restaurants")
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error {
+                fatalError("Failed to load persisten stores: \(error)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                NSLog("Error saving context: \(error)")
+                context.reset()
+            }
+        }
+    }
+    
+}
