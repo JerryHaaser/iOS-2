5306fd1887221f9d10eb5b5df98e856443c324b8
Jerry Haaser <jerry.haaser@hotmail.com>
2020-01-20 09:45:53 -0800

    added UItest

---
+++ FoodieFun/FoodieFun/ViewControllers/ReviewDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReviewDetailViewController.swift
+//  FoodieFun
+//
+//  Created by Jerry haaser on 1/20/20.
+//  Copyright © 2020 Jerry haaser. All rights reserved.
+//
+
+import UIKit
+
+class ReviewDetailViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var reviewTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FoodieFun/FoodieFunUnitTests/FoodieFunUnitTests.swift
+++ FoodieFun/FoodieFunUnitTests/FoodieFunUnitTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import FoodieFun
 
 class FoodieFunUnitTests: XCTestCase {
 
@@ -14,20 +15,11 @@ class FoodieFunUnitTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testNumberOfRows() {
+        let restaurantTableViewController = RestaurantsTableViewController()
+        let sectionInfo = restaurantTableViewController.fetchedResultsController.sections
+        let numberOfRows = sectionInfo?.compactMap { $0.numberOfObjects }
+        XCTAssertEqual(numberOfRows, [1])
     }
 
 }
